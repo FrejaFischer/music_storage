4dfa1a55d62b7fefedb3312f92ef8d9061252ebf
Freja Fischer <frej657e@stud.kea.dk>
2025-05-09 08:43:24 +0200

    updated deployed test, so POST works in deployed version also!

--- production/testing/script.js
+++ production/testing/script.js
@@ -1,5 +1,36 @@
 // Development
-// function testApi(e, endpoint, method = "GET", body = null, from = null) {
+function testApi(e, endpoint, method = "GET", body = null, from = null) {
+  const parentSection = e.closest("section");
+  const output = parentSection.querySelector(".output");
+
+  const options = {
+    method: method,
+  };
+
+  // Add FormData body for POST
+  if (body && method === "POST") {
+    const formData = new FormData();
+    for (const key in body) {
+      if (body.hasOwnProperty(key)) {
+        formData.append(key, body[key]);
+      }
+    }
+    options.body = formData;
+  }
+
+  fetch(`http://localhost:8888/exam/music_storage/public/${endpoint}`, options)
+    .then((response) => response.text())
+    .then((data) => {
+      if (output) output.textContent = data;
+    })
+    .catch((error) => {
+      if (output) output.textContent = "Error: " + error;
+    });
+}
+
+// Production
+// Fetch function with POST
+// function testApi(e, endpoint, method = "GET", body = null) {
 //   const parentSection = e.closest("section");
 //   const output = parentSection.querySelector(".output");
 
@@ -18,7 +49,7 @@
 //     options.body = formData;
 //   }
 
-//   fetch(`http://localhost:8888/exam/music_storage/public/${endpoint}`, options)
+//   fetch(`http://digital-media-api.infinityfreeapp.com/api/${endpoint}`, options)
 //     .then((response) => response.text())
 //     .then((data) => {
 //       if (output) output.textContent = data;
@@ -28,51 +59,21 @@
 //     });
 // }
 
-// Production
-function testApi(e, endpoint, method = "GET") {
-  fetch(`http://digital-media-api.infinityfreeapp.com/api/${endpoint}`, {
-    method: method,
-  })
-    .then((response) => response.text())
-    .then((data) => {
-      const parentSection = e.closest("section");
-      const output = parentSection.querySelector(".output");
-      if (output) output.textContent = data;
-    })
-    .catch((error) => {
-      const parentSection = e.closest("section");
-      const output = parentSection.querySelector(".output");
-      if (output) output.textContent = error;
-    });
-}
-
-// Fetch function with POST (breaks deployed testing)
-// function testApi(e, endpoint, method = "GET", body = null) {
-//   const parentSection = e.closest("section");
-//   const output = parentSection.querySelector(".output");
-
-//   const options = {
+// Simple solution
+// function testApi(e, endpoint, method = "GET") {
+//   fetch(`http://digital-media-api.infinityfreeapp.com/api/${endpoint}`, {
 //     method: method,
-//   };
-
-//   // Add FormData body for POST
-// if (body && method === "POST") {
-//   const formData = new FormData();
-//   for (const key in body) {
-//     if (body.hasOwnProperty(key)) {
-//       formData.append(key, body[key]);
-//     }
-//   }
-//   options.body = formData;
-// }
-
-//   fetch(`https://digital-media-api.infinityfreeapp.com/api/${endpoint}`, options)
+//   })
 //     .then((response) => response.text())
 //     .then((data) => {
+//       const parentSection = e.closest("section");
+//       const output = parentSection.querySelector(".output");
 //       if (output) output.textContent = data;
 //     })
 //     .catch((error) => {
-//       if (output) output.textContent = "Error: " + error;
+//       const parentSection = e.closest("section");
+//       const output = parentSection.querySelector(".output");
+//       if (output) output.textContent = error;
 //     });
 // }
 
@@ -84,11 +85,11 @@ document.querySelector(".search_artist").addEventListener("click", (e) => testAp
 document.querySelector(".artist").addEventListener("click", (e) => testApi(e.currentTarget, "artists/30?api_key=abcd1234"));
 document.querySelector(".artist_albums").addEventListener("click", (e) => testApi(e.currentTarget, "artists/24/albums?api_key=abcd1234"));
 document.querySelector(".artist_delete").addEventListener("click", (e) => testApi(e.currentTarget, "artists/30?api_key=abcd1234", "DELETE"));
-// document.querySelector(".artist_add").addEventListener("click", (e) =>
-//   testApi(e.currentTarget, "artists?api_key=abcd1234", "POST", {
-//     name: "New Artist",
-//   })
-// );
+document.querySelector(".artist_add").addEventListener("click", (e) =>
+  testApi(e.currentTarget, "artists?api_key=abcd1234", "POST", {
+    name: "New Artist",
+  })
+);
 
 // Albums
 document.querySelector(".all_albums").addEventListener("click", (e) => testApi(e.currentTarget, "albums?api_key=abcd1234"));
@@ -96,18 +97,18 @@ document.querySelector(".search_albums").addEventListener("click", (e) => testAp
 document.querySelector(".album").addEventListener("click", (e) => testApi(e.currentTarget, "albums/10?api_key=abcd1234"));
 document.querySelector(".albums_tracks").addEventListener("click", (e) => testApi(e.currentTarget, "albums/24/tracks?api_key=abcd1234"));
 document.querySelector(".album_delete").addEventListener("click", (e) => testApi(e.currentTarget, "albums/348?api_key=abcd1234", "DELETE"));
-// document.querySelector(".album_add").addEventListener("click", (e) =>
-//   testApi(e.currentTarget, "albums?api_key=abcd1234", "POST", {
-//     title: "New album",
-//     artist_id: 2,
-//   })
-// );
-// document.querySelector(".album_update").addEventListener("click", (e) =>
-//   testApi(e.currentTarget, "albums/1?api_key=abcd1234", "POST", {
-//     title: "New title",
-//     artist_id: 2,
-//   })
-// );
+document.querySelector(".album_add").addEventListener("click", (e) =>
+  testApi(e.currentTarget, "albums?api_key=abcd1234", "POST", {
+    title: "New album",
+    artist_id: 2,
+  })
+);
+document.querySelector(".album_update").addEventListener("click", (e) =>
+  testApi(e.currentTarget, "albums/10?api_key=abcd1234", "POST", {
+    title: "New title",
+    artist_id: 2,
+  })
+);
 
 // Clear
 document.querySelectorAll(".clear").forEach((el) => {
